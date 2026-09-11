4f41b8106706d477d5e38ec4f684cdc6e241ebba
3fr0sty <20278283@farn-ct.ac.uk>
2026-09-11 08:13:56 +0100

    feat: image appears after clicking button on about.html. feat: contact form returns message after submission

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,8 +1,22 @@
-alert("Javascript is working!");
 console.log("Javascript is working!");
 
 let button = document.getElementById("testButton");
-button.addEventListener("click",function() {
-    let buttonValue = button.value;
-    console.log(buttonValue+" button was clicked!");
-});
+if (button) {
+    button.addEventListener("click",function() {
+        let buttonValue = button.value;
+        console.log(buttonValue+" button was clicked!");
+        let image = document.getElementById("goodison");
+        image.hidden = !image.hidden;
+    });
+}
+
+let form = document.getElementById("contactForm");
+if (form) {
+    let message = document.getElementById("formMessage");
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+        let name = document.getElementById("name").value;
+        message.textContent = "Thank you " + name;
+        message.hidden = false;
+    });
+}
